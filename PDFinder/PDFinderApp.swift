09d1e1adbb754b40558b90dc48eb32b85cad1304
Ivan <i.a.konuhov@gmail.com>
2023-04-15 14:18:40 +0700

    Initial Commit

---
+++ PDFinder/PDFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFinderApp.swift
+//  PDFinder
+//
+//  Created by Иван on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PDFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
